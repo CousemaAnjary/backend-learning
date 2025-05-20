24b6d8bcb0feab004ee76bfac0e13240750c22b5
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-20 20:24:50 +0300

    Ajout de la récupération des données utilisateur dans la méthode d'enregistrement du contrôleur d'authentification

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -3,8 +3,9 @@ import { Request, Response } from "express"
 module.exports = {
   async register(req: Request, res: Response) {
     try {
+      const { name, email, password } = req.body
 
-
+      
     } catch (error) {
       res.status(500).json({ message: "Internal server error" })
     }
