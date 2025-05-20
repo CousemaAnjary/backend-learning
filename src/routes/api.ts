78669758ff149ed441021b8ce50402c22ed1dcfe
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-20 17:02:05 +0300

    Création du contrôleur d'authentification et définition de la route d'enregistrement des utilisateurs

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,8 +2,9 @@ import express from "express"
 const router = express.Router()
 
 // Importation des contrôleurs
+const Auth = require("../controllers/Auth")
 
 // Définition des routes
-
+router.post("/register", Auth.register)
 
 module.exports = router
