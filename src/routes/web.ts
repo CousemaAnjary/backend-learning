5a2d25b7bef5344cc15af2859d3e66424bee9a43
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-20 16:46:38 +0300

    Ajout de la définition de la route principale dans le fichier web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -3,4 +3,9 @@ const router = express.Router()
 
 // Importation des contrôleurs
 
+// Définition des routes
+router.get("/", (req, res) => {
+  res.send("Hello World! This is the web route.")
+})
+
 module.exports = router
