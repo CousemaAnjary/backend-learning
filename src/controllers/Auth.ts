49a911580605d4baf651a3c2d4933b787bf71397
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-20 21:19:34 +0300

    Ajout de la validation des données d'entrée dans la méthode d'enregistrement et mise à jour de la configuration de la base de données

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express"
+import { registerSchema } from "../schema/auth.schema"
+import db from "../db/drizzle"
 
 module.exports = {
   async register(req: Request, res: Response) {
     try {
-      const { name, email, password } = req.body
+      // Validation des données d'entrée (Zod)
+      const validatedData = registerSchema.safeParse(req.body)
+      if (!validatedData.success) return res.status(400).json({ success: false, errors: validatedData.error.errors }) 
 
-      
+     // Destructuration des données validées
+      const { name, email, password, image } = validatedData.data
+
+      // Vérification si l'utilisateur existe déjà
+      const existingUser = await db.query.users
+
     } catch (error) {
       res.status(500).json({ message: "Internal server error" })
     }
