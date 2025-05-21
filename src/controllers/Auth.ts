96309627b12d50d69a0a01a3397a1300e54ed1c3
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-21 16:53:38 +0300

    Ajout de la gestion de l'authentification avec JWT et création d'un schéma de validation pour la connexion

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,10 +1,12 @@
 import bcrypt from "bcryptjs"
-import db from "../db/drizzle"
 import { eq } from "drizzle-orm"
-import { users } from "../db/schema"
 import { Request, Response } from "express"
-import { registerSchema } from "../schema/auth.schema"
+import jwt from "jsonwebtoken"
+import db from "../db/drizzle"
+import { users } from "../db/schema"
+import { loginSchema, registerSchema } from "../schema/auth.schema"
 
+const JWT_SECRET = process.env.JWT_SECRET as string 
 
 module.exports = {
   async register(req: Request, res: Response) {
@@ -38,4 +40,34 @@ module.exports = {
       res.status(500).json({ message: "Internal server error" })
     }
   },
+
+  async login(req: Request, res: Response)  {
+    try {
+      // Validation des données d'entrée (Zod)
+      const validatedData = loginSchema.safeParse(req.body)
+      if (!validatedData.success) return res.status(400).json({ success: false, error: validatedData.error.format() })
+
+      // Destructuration des données validées
+      const { email, password } = validatedData.data
+
+      // Recherche de l'utilisateur dans la base de données
+      const user = await db.query.users.findFirst({ where: eq(users.email, email) })
+      if (!user) return res.status(401).json({ success: false, message: "Invalid email" })
+
+      // Comparer les mot de passe
+      const isPasswordValid = await bcrypt.compare(password, user.password)
+      if (!isPasswordValid) return res.status(401).json({ success: false, message: "Invalid password" })
+
+      // Générer un token JWT (à implémenter)
+      const token = jwt.sign({ id: user.id, email: user.email , role:user.role }, JWT_SECRET, { expiresIn: "1h" })
+
+      // Retourner une réponse de succès avec le token
+      return res.status(200).json({success: true, message: "Login successful", token })
+
+
+
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" })
+    }
+  }
 }
