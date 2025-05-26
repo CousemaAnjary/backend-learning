7b9ce6041207e14f6f69462cf8d52944fafe2d20
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-26 15:41:05 +0300

    Ajout de la route pour la création de catégories

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,7 @@ const router = express.Router()
 
 // Importation des contrôleurs
 const Auth = require("../controllers/Auth")
+const Categories = require("../controllers/Categories")
 
 // Définition des routes
 
@@ -11,6 +12,6 @@ router.post("/register", Auth.register)
 router.post("/login", Auth.login)
 
 // Route pour catégories
-
+router.post("/category", Categories.createCategory)
 
 module.exports = router
