023d88e244cc6b7eff44ac36ce75c4e52bf97d53
CousemaAnjary <anjaryabdillah@gmail.com>
2025-05-26 15:52:20 +0300

    Ajout de la colonne user_id et de la contrainte de clé étrangère dans la table books

--- src/db/schema/books.ts
+++ src/db/schema/books.ts
@@ -1,3 +1,4 @@
+import { users } from "./auth";
 import { categories } from "./categories"
 import { boolean, pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core"
 
@@ -7,6 +8,9 @@ export const books = pgTable("books", {
   author: text("author").notNull(),
   description: text("description"),
   coverImage: text("cover_image"),
+
   categoryId: uuid("category_id").notNull().references(() => categories.id),
+  userId: uuid("user_id").notNull().references(() => users.id),
+
   createdAt: timestamp("created_at").notNull().defaultNow(),
 });
